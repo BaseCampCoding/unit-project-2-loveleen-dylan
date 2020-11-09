93ad1fd3da26d8a7b8d5e3a5b914336191959388
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-09 11:29:51 -0600

    welcome greeting

--- mian.py
+++ mian.py
@@ -17,3 +17,28 @@ print(
 _____________________________________L___________J________________________
 --------------------------------------------------------------------------"""
 )
+
+print(
+    """
+    WELCOME TO ReservME WHERE YOU RESERVE FOR A ME! 
+
+    We are delighted to have you here with us today.
+
+    Let's get started!
+    """
+)
+
+print(
+    """
+    We have 5 types of rooms.
+    
+    Studio Suite:
+
+    One-Bedroom Suite:
+
+    Two-Bedroom Suite:
+
+    Presidential Suite:
+    
+    Premium Suite:
+)
