b87e081a080d6271330c82e50a1d73e52bff7764
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-09 11:22:58 -0600

    created rooms availibity table and main.py

--- database_setup.py
+++ database_setup.py
@@ -1,8 +1,11 @@
 import sqlite3
 
-con = sqlite3.connect("Customer_Info.db")
+con = sqlite3.connect("RESERVEME.db")
 cur = con.cursor()
 
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT"
+    "CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT)"
+)
+
+cur.execute("CREATE TABLE IF NOT EXISTS RoomsAvailability(roomID AUTO_INCREMENT, availability TEXT, room_type TEXT, price REAL, size INT, guest TEXT)"
 )
