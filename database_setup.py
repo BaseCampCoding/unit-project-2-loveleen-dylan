6e7ed2e24b925af086359bc44439c8d7d469ca0b
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-09 14:22:32 -0600

    fixed no such table error, variable issues, insert RoomsAvailability

--- database_setup.py
+++ database_setup.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-con = sqlite3.connect("RESERVEME.db")
+con = sqlite3.connect("RESERVME.db")
 cur = con.cursor()
 
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT)"
+cur.execute("CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT)"
 )
 
 cur.execute("CREATE TABLE IF NOT EXISTS RoomsAvailability(roomID AUTO_INCREMENT, availability TEXT, room_type TEXT, price REAL, size INT, guest TEXT)"
 )
+con.commit()
+
