0e3f09c926a0f5812b1cace261d39e44ac04ff58
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2020-11-10 14:10:16 -0600

    3rd block push

--- main.py
+++ main.py
@@ -1,83 +1,61 @@
 import sqlite3
+from tkinter import *
+from PIL import Image, ImageTk
 
 
-print(
-    """
-    
-                                RESERVEME
-                  . .                     -:-             .  .  .
-            .'.:,'.        .  .  .     ' .           . \ | / .
-            .'.;.`.       ._. ! ._.       \          .__\:/__.
-             `,:.'         ._\!/_.                     .';`.      . ' .
-             ,'             . ! .        ,.,      ..======..       .:.
-            ,                 .         ._!_.     ||::: : | .        ',
-     .====.,                  .           ;  .~.===: : : :|   ..===.
-     |.::'||      .=====.,    ..=======.~,   |"|: :|::::::|   ||:::|=====|
-  ___| :::|!__.,  |:::::|!_,   |: :: ::|"|l_l|"|:: |:;;:::|___!| ::|: : :|
- |: :|::: |:: |!__|; :: |: |===::: :: :|"||_||"| : |: :: :|: : |:: |:::::|
- |:::| _::|: :|:::|:===:|::|:::|:===F=:|"!/|\!"|:::|:====:|::_:|: :|::__:|
- !_[]![_]_!_[]![]_!_[__]![]![_]![_][I_]!//_:_\\![]I![_][_]!_[_]![]_!_[__]!
- -----------------------------------"---''''```---"-----------------------
- _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ |= _ _:_ _ =| _ _ _ _ _ _ _ _ _ _ _ _
-                                     |=    :    =|                      
-_____________________________________L___________J________________________
---------------------------------------------------------------------------
+con = sqlite3.connect("RESERVME.db")
+c = con.cursor()
 
-
-"""
+root = Tk()
+root.title(" Rooms")
+root.iconbitmap(
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-bkeqe&psig=AOvVaw2oBqXabpCHoyEx9EyMKvdl&ust=1605115375818000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDPvfG--OwCFQAAAAAdAAAAABAh"
 )
+root.geometry("200x100")
 
-print(
-    """
-WELCOME TO ReservME WHERE YOU RESERVE FOR A ME! 
+# room_t = Entry(root, width=30)
+# room_t.grid(row=0, column=1)
+# room_population = Entry(root, width=30)
+# room_population.grid(row=1, column=1)
+# price = Entry(root, width=30)
+# price.grid(row=2, column=1)
 
-We are delighted to have you here with us today.
-
-Let's get started!
-"""
-)
-print("We have 5 types of rooms.")
-print("\n" + "\033[1m" + "Studio Suite: ")
-print(
-    "\033[0m"
-    + "Featuring a plush bed and living space with extra seating, a sleeper sofa, and a TV that can be seen from every angle of the suite. Each studio suite includes a workstation, a wet bar, a refrigerator, and a microwave."
-)
-print("$149")
-print("\n\n" + "\033[1m" + "One-Bedroom Suite: ")
-print(
-    "\033[0m"
-    + "Including a separate living room with sleeper sofa and a bedroom with a TV in both spaces. Each one-bedroom suite features a workstation, a wet bar, a refrigerator, and a microwave."
-)
-print("$99")
-print("\n\n" + "\033[1m" + "Two-Bedroom Suite: ")
-print(
-    "\033[0m"
-    + "Spread out in the separate living room with sleeper sofa and two bedrooms with a TV in all three spaces. Each two-bedroom suite includes a workstation, a wet bar, a refrigerator, and a microwave."
-)
-print("$154")
-print("\n\n" + "\033[1m" + "Presidential Suite: ")
-print(
-    "\033[0m"
-    + "Experience even more space and comfort in our Presidential Suite. Some hotels’ Presidential Suites feature an espresso machine, fireplace, a luxury bathroom feature like whirlpool tubs, or a conference table with seating for up to eight."
-)
-print("$259")
-print("\n\n" + "\033[1m" + "Premium Suite: ")
-print(
-    "\033[0m"
-    + "Offering the same experience as our popular one-bedroom suites, Premium Suites include additional upgrades of Premium WiFi, a Keurig coffee brewer, plus snacks and drinks which are replenished daily."
-)
-print("$324")
+# room_t_label = Label(root, text="Room Type")
+# room_t_label.grid(row=0, column=0)
+# room_population_label = Label(root, text="Room Capacity")
+# room_population_label.grid(row=1, column=0)
+# price_label = Label(root, text="Price")
+# price_label.grid(row=2, column=0)
+room_label_entry = Entry(root, width=30)
+room_label_entry.grid(row=2, column=0)
+room_label = Label(root, text="Room Type")
+room_label.grid(row=1, column=0)
 
 
+def submit():
+    root = Tk()
+    room_label_get = room_label_entry.get()
+    root.title(" Available Rooms")
+    root.iconbitmap(
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-bkeqe&psig=AOvVaw2oBqXabpCHoyEx9EyMKvdl&ust=1605115375818000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDPvfG--OwCFQAAAAAdAAAAABAh"
+    )
+    root.geometry("300x300")
+    c.execute(
+        "SELECT roomID,availability,room_type,price FROM RoomsAvailability WHERE room_type =(?) AND  availability = 'Available'",
+        (room_label_get,),
+    )
+    rooms = c.fetchall()
 
-room_type = input("\n" + "What type of room would you like to reserve today?: ")
-input_room_type = room_type.lower()
-for i in ROOMS:
-    if input_room_type not in ROOMS:
-        print("That room does not exist. Please try again.")
-        room_type = input("\n" + "What type of room would you like to reserve today?: ")
+    print_info = ""
+    for info in rooms:
+        print_info += str(info) + "\n"
 
-print("Great. Before you can reserve a hotel room we need some information.")
+    query_label = Label(root, text=print_info)
+    query_label.grid(row=0, column=0, columnspan=1)
 
-con.commit()
-con.close()
+
+submit_btn = Button(root, text="Submit", command=submit)
+submit_btn.grid(row=6, column=0, columnspan=1, ipady=5, ipadx=69)
+
+
+root.mainloop()
