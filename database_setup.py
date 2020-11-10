d96bed8125cf96eb2715f1dc351d5867b408e2b0
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-10 10:39:31 -0600

    database setup for roomsavailability database

--- database_setup.py
+++ database_setup.py
@@ -9,6 +9,62 @@ cur.execute("CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address 
 cur.execute("CREATE TABLE IF NOT EXISTS RoomsAvailability(roomID AUTO_INCREMENT, availability TEXT, room_type TEXT, price REAL, size INT, guest TEXT)"
 )
 
+# cur.execute("""INSERT INTO RoomsAvailability(room_type, price) VALUES
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("Studio Suite", 149),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("One-Bedroom Suite", 99),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Two-Bedroom Suite", 154),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Presidential Suite", 259),
+#     ("Premium Suite", 324), 
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324),
+#     ("Premium Suite", 324)
+#     """)
+
+# cur.execute("SELECT * FROM RoomsAvailability")
+# print(cur.fetchall())
+
 cur.execute("CREATE TABLE IF NOT EXISTS Rooms(room_type TEXT, capacity INT, price REAL)")
 
 # cur.execute("""INSERT INTO Rooms VALUES 
@@ -19,6 +75,6 @@ cur.execute("CREATE TABLE IF NOT EXISTS Rooms(room_type TEXT, capacity INT, pric
 #     ("Premium Suite", 7, 324)
 #     """)
 
-cur.execute("SELECT * FROM Rooms")
-print(cur.fetchall())
+# cur.execute("SELECT * FROM Rooms")
+# print(cur.fetchall())
 
