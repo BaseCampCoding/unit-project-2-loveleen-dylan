3e0c396ba4e172db534fc6c22d5f762d41215e26
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-10 09:24:03 -0600

    fixed erase error

--- classes.py
+++ classes.py
@@ -1,4 +1,23 @@
-R  number_guests = input_number_check("How many guests will there be in total?: ")
+def input_number_check(prompt: str) -> int:
+    while True:
+        response = input(prompt)
+        if response.isdigit():
+            response = int(response)
+            if response >= 0:
+                return response
+        print("Please provide a valid input.")
+        
+class Rooms:
+    def __init__(self, type, description, price, inquiries):
+    self.type = room_type
+    self.description = description
+    self.price = price
+
+    def inquiries():
+    full_name = input("What is your name?: ")
+    will_there_be_guests = input("Will there be any other guests?(Y/N): ")
+    if will_there_be_guests.lower() == "y":
+        number_guests = input_number_check("How many guests will there be in total?: ")
         guests = []
         for i in range(len(number_guests)):
             name_of_guest = input("Enter Guest Name: ")
