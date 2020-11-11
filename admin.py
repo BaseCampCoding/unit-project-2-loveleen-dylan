f12838c507fe117c29d823837a7af7534ebc1fc9
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2020-11-11 10:29:25 -0600

    GUI changes for database

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ from tkinter import *
 def main_control():
     root = Tk()
     root.title("Admin Control")
-    root.geometry("400x167")
+    root.geometry("400x187")
 
     # Query function to insert data
     def query():
@@ -16,8 +16,9 @@ def main_control():
 
         # Insert into table
         c.execute(
-            "INSERT INTO UserInformation VALUES(:f_name, :address, :email, :zipcode, :state, :phoneNumber)",
+            "INSERT INTO UserInformation VALUES(:room_type, :f_name, :address, :email, :zipcode, :state, :phoneNumber)",
             {
+                "room_type": room_type.get(),
                 "f_name": f_name.get(),
                 "address": address.get(),
                 "email": email.get(),
@@ -26,14 +27,14 @@ def main_control():
                 "phoneNumber": phoneNumber.get(),
             },
         )
-
-        c.execute("INSERT INTO RoomsAvailability(guest) VALUES(?)", (f_name.get(),))
-
+        con.commit()
+        c.execute("UPDATE RoomsAvailability SET guest = (?)", (f_name.get(),))
         # Commit data
         con.commit()
         # Close database
         con.close()
         # Clear Text Boxes
+        room_type.delete(0, END)
         f_name.delete(0, END)
         address.delete(0, END)
         email.delete(0, END)
@@ -41,33 +42,54 @@ def main_control():
         state.delete(0, END)
         phoneNumber.delete(0, END)
 
+    # Function to show data
+    def records():
+        # Connect to database
+        con = sqlite3.connect("RESERVME.db")
+        # Cursor
+        c = con.cursor()
+
+        # Select data
+        c.execute("SELECT * FROM UserInformation")
+
+        # Commit changes
+        con.commit()
+
+        # Close Connection
+        con.close()
+
     # Create Text Boxes
+    room_type = Entry(root, width=30)
+    room_type.grid(row=0, column=1)
     f_name = Entry(root, width=30)
-    f_name.grid(row=0, column=1)
+    f_name.grid(row=1, column=1)
     address = Entry(root, width=30)
-    address.grid(row=1, column=1)
+    address.grid(row=2, column=1)
     email = Entry(root, width=30)
-    email.grid(row=2, column=1)
+    email.grid(row=3, column=1)
     zipcode = Entry(root, width=30)
-    zipcode.grid(row=3, column=1)
+    zipcode.grid(row=4, column=1)
     state = Entry(root, width=30)
-    state.grid(row=4, column=1)
+    state.grid(row=5, column=1)
     phoneNumber = Entry(root, width=30)
-    phoneNumber.grid(row=5, column=1)
+    phoneNumber.grid(row=6, column=1)
 
     # Create Text Box Labels
+    room_type_label = Label(root, text="Room Type")
+    room_type_label.grid(row=0, column=0)
     f_name_label = Label(root, text="Full Name")
-    f_name_label.grid(row=0, column=0)
+    f_name_label.grid(row=1, column=0)
     address_label = Label(root, text="Address")
-    address_label.grid(row=1, column=0)
+    address_label.grid(row=2, column=0)
     email_label = Label(root, text="Email")
-    email_label.grid(row=2, column=0)
+    email_label.grid(row=3, column=0)
     zipcode_label = Label(root, text="Zipcode")
-    zipcode_label.grid(row=3, column=0)
+    zipcode_label.grid(row=4, column=0)
     state_label = Label(root, text="State")
-    state_label.grid(row=4, column=0)
+    state_label.grid(row=5, column=0)
     phoneNumber_label = Label(root, text="Phone Number")
-    phoneNumber_label.grid(row=5, column=0)
+    phoneNumber_label.grid(row=6, column=0)
 
     submit_btn = Button(root, text="Add to Database", command=query)
-    submit_btn.grid(row=6, column=1, columnspan=1, ipady=5, ipadx=69)
+    submit_btn.grid(row=7, column=1, columnspan=1, ipady=5, ipadx=69)
+    show_records = Button(root, text="Show Records,")
--- database_setup.py
+++ database_setup.py
@@ -4,74 +4,72 @@ con = sqlite3.connect("RESERVME.db")
 cur = con.cursor()
 
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS UserInformation(full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT)"
+    "CREATE TABLE IF NOT EXISTS UserInformation(room_type TEXT, full_name TEXT, address TEXT, email TEXT, zipcode INT, state TEXT, phoneNumber INT)"
 )
 
 cur.execute(
     "CREATE TABLE IF NOT EXISTS RoomsAvailability(roomID INTEGER PRIMARY KEY, availability TEXT DEFAULT 'Available', room_type TEXT, price REAL, size INT, guest TEXT)"
 )
 
-# cur.execute(
-#     """INSERT INTO RoomsAvailability(room_type, price) VALUES
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("Studio Suite", 149),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("One-Bedroom Suite", 99),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Two-Bedroom Suite", 154),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Presidential Suite", 259),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324),
-#     ("Premium Suite", 324)
-#     """
-# )
-# con.commit()
-cur.execute("SELECT room_type,roomID,availability FROM RoomsAvailability")
-print(cur.fetchall())
+cur.execute(
+    """INSERT INTO RoomsAvailability(room_type, price) VALUES
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("Studio Suite", 149),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("One-Bedroom Suite", 99),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Two-Bedroom Suite", 154),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Presidential Suite", 259),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324),
+    ("Premium Suite", 324)
+    """
+)
+con.commit()
 
-# cur.execute(
-#     "CREATE TABLE IF NOT EXISTS Rooms(room_type TEXT, capacity INT, price REAL)"
-# )
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS Rooms(room_type TEXT, capacity INT, price REAL)"
+)
 
 # cur.execute(
 #     """INSERT INTO Rooms VALUES
@@ -82,6 +80,3 @@ print(cur.fetchall())
 #     ("Premium Suite", 7, 324)
 #     """
 # )
-
-# cur.execute("SELECT * FROM Rooms")
-# print(cur.fetchall())
