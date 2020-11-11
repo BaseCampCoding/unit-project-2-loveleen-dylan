676552e257462d6a2b4a8500ab5e1e2653b39c03
loveleen-kaur21 <72216914+loveleen-kaur21@users.noreply.github.com>
2020-11-11 09:38:36 -0600

    working on validation box for room type

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sqlite3
 from classes import Rooms
 from tkinter import *
+from tkinter import messagebox
 # from PIL import Image, ImageTk
+
+
 
 
 con = sqlite3.connect("RESERVME.db")
@@ -62,10 +65,30 @@ room_label_entry.grid(row=2, column=0)
 room_label = Label(root, text="Room Type")
 room_label.grid(row=1, column=0)
 
+ROOMS= ['studio suite', 'one-bedroom suite', 'two-bedroom suite', 'presidential suite', 'premium suite']
+
 
 def submit():
     root = Tk()
     room_label_get = room_label_entry.get()
+    input_room_type = room_label_get.lower()
+    if input_room_type not in ROOMS:
+        messagebox.showerror("Error", "The room you are looking for is not available. Please try again.")
+    if input_room_type == "studio suite":
+        room = "Studio Suite"
+        return room
+    elif input_room_type == "one-bedroom suite":
+        room = "One-Bedroom Suite"
+        return room
+    elif input_room_type == "two-bedroom suite":
+        room_type = "Two-Bedroom Suite"
+        return room
+    elif input_room_type == "presidential suite":
+        room_type = "Presidential Suite"
+        return room
+    elif input_room_type == "premium suite":
+        room_type = "Premium Suite"
+        return room
     root.title(" Available Rooms")
     root.iconbitmap(
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Ffree-png-bkeqe&psig=AOvVaw2oBqXabpCHoyEx9EyMKvdl&ust=1605115375818000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDPvfG--OwCFQAAAAAdAAAAABAh"
@@ -73,7 +96,7 @@ def submit():
     root.geometry("300x300")
     c.execute(
         "SELECT roomID,availability,room_type,price FROM RoomsAvailability WHERE room_type =(?) AND  availability = 'Available'",
-        (room_label_get,),
+        (room,),
     )
     rooms = c.fetchall()
 
